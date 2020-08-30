30e5c8173827beaa364ba46d47bc0dbb606234af
Misaki <45246543+kageroukw@users.noreply.github.com>
2020-08-30 02:39:14 +0200

    Added some empty lines for clarity

--- get.sh
+++ get.sh
@@ -3,8 +3,11 @@ BASE=$PWD
 echo ""
 echo "Initiated..."
 echo ""
+
 echo "Downloading Quote Linux Launcher......."
 curl https://raw.githubusercontent.com/Quote-Bot/Quote-Scripts/master/quote_launcher.sh -o quote_launcher.sh && trap dos2unix ERR
 echo "Done! Self eliminating..."
+
 rm -rf $BASE/get.sh
+
 exit 1
