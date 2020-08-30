bbc6faaf2e14f6af9cf0ff0d6a7df8d4a3cc45d5
Misaki <45246543+kageroukw@users.noreply.github.com>
2020-08-30 22:03:19 +0200

    Added AIOHTTP and PIP requirements

--- get.sh
+++ get.sh
@@ -28,6 +28,22 @@ else
 	echo "$(tput setaf 2)dos2unix is installed$(tput setaf 7)"
 fi
 
+if ! hash pip &> /dev/null;
+then
+	echo "$(tput setaf 3)pip is not installed$(tput setaf 7)"
+	sudo apt-get install python-pip
+else
+	echo "$(tput setaf 2)pip is installed$(tput setaf 7)"
+fi
+
+if ! hash aiohttp &> /dev/null;
+then
+	echo "$(tput setaf 3)aiohttp is not installed$(tput setaf 7)"
+	sudo apt-get install aiohttp
+else
+	echo "$(tput setaf 2)aiohttp is installed$(tput setaf 7)"
+fi
+
 echo "Downloading Quote Linux Launcher......."
 curl https://raw.githubusercontent.com/Quote-Bot/Quote-Scripts/master/quote_launcher.sh -o quote_launcher.sh
 dos2unix quote_launcher.sh
