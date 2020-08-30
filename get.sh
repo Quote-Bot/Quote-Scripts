b81bbefb7e8da7effa5d8b859db1576c76af2621
Misaki <45246543+kageroukw@users.noreply.github.com>
2020-08-30 02:41:49 +0200

    Moved dos2unix to new line

--- get.sh
+++ get.sh
@@ -5,7 +5,8 @@ echo "Initiated..."
 echo ""
 
 echo "Downloading Quote Linux Launcher......."
-curl https://raw.githubusercontent.com/Quote-Bot/Quote-Scripts/master/quote_launcher.sh -o quote_launcher.sh && trap dos2unix ERR
+curl https://raw.githubusercontent.com/Quote-Bot/Quote-Scripts/master/quote_launcher.sh -o quote_launcher.sh
+dos2unix quote_launcher.sh
 echo "Done! Self eliminating..."
 
 rm -rf $BASE/get.sh
