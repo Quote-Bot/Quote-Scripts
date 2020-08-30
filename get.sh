80b022d70ae3a2844c52ba060cd53234634c395e
Misaki <45246543+kageroukw@users.noreply.github.com>
2020-08-30 15:17:11 +0200

    Added requirements

--- get.sh
+++ get.sh
@@ -4,6 +4,30 @@ echo ""
 echo "Initiated..."
 echo ""
 
+if ! hash python3 2> /dev/null;
+then
+	echo "$(tput setaf 3)Python 3.5+ is not installed$(tput setaf 7)"
+	sudo apt-get install python3
+else
+	echo "$(tput setaf 2)Python 3.5+ is installed$(tput setaf 7)"
+fi
+
+if ! hash git &> /dev/null;
+then
+	echo "$(tput setaf 3)Git is not installed$(tput setaf 7)"
+	sudo apt-get install git
+else
+	echo "$(tput setaf 2)Git is installed$(tput setaf 7)"
+fi
+
+if ! hash dos2unix &> /dev/null;
+then
+	echo "$(tput setaf 3)dos2unix is not installed$(tput setaf 7)"
+	sudo apt-get install dos2unix
+else
+	echo "$(tput setaf 2)dos2unix is installed$(tput setaf 7)"
+fi
+
 echo "Downloading Quote Linux Launcher......."
 curl https://raw.githubusercontent.com/Quote-Bot/Quote-Scripts/master/quote_launcher.sh -o quote_launcher.sh
 dos2unix quote_launcher.sh
